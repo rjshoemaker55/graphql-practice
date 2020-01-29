59d289310347375e0349f5e9300195fe2c9b4be7
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-01-29 18:15:17 -0500

    back end mvp complete

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,7 +10,8 @@ const {
   GraphQLSchema,
   GraphQLID,
   GraphQLInt,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -101,8 +102,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) }
       },
       resolve(parent, args) {
         let author = new Author({
@@ -115,9 +116,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        authorId: { type: GraphQLID },
-        genre: { type: GraphQLString }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
+        genre: { type: new GraphQLNonNull(GraphQLString) }
       },
       resolve(parent, args) {
         let book = new Book({
